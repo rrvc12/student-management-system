d2e277bc43b9d11b2bb078f736647f0d58e6a031
Raúl Ruilova <rruilova12@gmail.com>
2023-10-22 01:16:46 -0500

    [add] creamos proyecto con django

--- students/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Student
-
-
-# Create your views here.
-def index(request):
-    return render(request, 'students/index.html', {
-        'students': Student.objects.all()
-    })
